b2de01efa45ece177e72c6678c6f99b28b795ef7
Assol Nurbaeva <nurbaevaassol@gmail.com>
2022-09-07 17:49:26 +0300

    first commit

---
+++ check.js
@@ -0,0 +1,10 @@
+const d = prompt('Введите число');
+const i = prompt('Введите число');
+if (d >=i){
+    alert(`${d} Бщльше чем ${i}`);
+
+}else if (d <=i){
+    alert(`${d} Меньше чем ${i}`)
+}else if (d = i){
+    alert('Равны')
+}
---
+++ greeting.js
@@ -0,0 +1,5 @@
+const v = prompt('What is your name?')
+const c = prompt('What is your last name')
+alert(`hello, ${v} ${c}`)
+
+ 
